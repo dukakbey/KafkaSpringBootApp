a8a3d7879fcec5c1a0a11f615f8418789797ea1a
dukakbey <sceylan@gmail.com>
2024-09-17 23:26:11 +0300

    -

--- kafka-data-receiver/src/main/java/org/sceylan/kafka_data_receiver/service/KafkaConsumerService.java
+++ kafka-data-receiver/src/main/java/org/sceylan/kafka_data_receiver/service/KafkaConsumerService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumerService {
  
-    @KafkaListener(topics = "customer_data",groupId = "barisdere1")
+    @KafkaListener(topics = "customer_data")
     public static void receiveData(String m)
     {
         System.out.println("Received Message: " + m);
