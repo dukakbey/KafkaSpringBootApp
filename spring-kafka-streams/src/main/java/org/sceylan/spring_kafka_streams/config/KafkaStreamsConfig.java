fd501ce923ef9f7c6e2196d375f67622bfe09792
dukakbey <96448595+dukakbey@users.noreply.github.com>
2024-09-18 15:48:48 +0300

    -

--- spring-kafka-streams/src/main/java/org/sceylan/spring_kafka_streams/config/KafkaStreamsConfig.java
+++ spring-kafka-streams/src/main/java/org/sceylan/spring_kafka_streams/config/KafkaStreamsConfig.java
@@ -1,37 +1,29 @@
 package org.sceylan.spring_kafka_streams.config;
 
-import java.util.Properties;
-
-import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsBuilder;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Produced;
-import org.apache.kafka.streams.kstream.ValueMapper;
-import org.sceylan.spring_kafka_streams.bean.Customer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
 
 @Configuration
+@EnableKafka
+@EnableKafkaStreams
 public class KafkaStreamsConfig {
 
-    @Bean
-    public KafkaStreams kafkaStreams()
+    @Bean(name = "defaultKafkaStreamsConfig")
+    public KafkaStreamsConfiguration kafkaStreamConfig()
     {
-        Properties props = new Properties();
+        Map<String, Object> props = new HashMap<>();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "customer-streams-app");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-1:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, org.apache.kafka.common.serialization.Serdes.String().getClass().getName());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, org.springframework.kafka.support.serializer.JsonSerde.class.getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, org.springframework.kafka.support.serializer.JsonSerde.class.getName());       
 
-        StreamsBuilder builder = new StreamsBuilder();
-        KStream<String,Customer> customerStream = builder.stream("customer_data");
-        customerStream
-            .mapValues((ValueMapper<Customer, String>) customer -> "Name length: " + customer.getFirstName().length() + customer.getLastName().length())
-            .to("updated_data", Produced.with(org.apache.kafka.common.serialization.Serdes.String(), org.apache.kafka.common.serialization.Serdes.String()));
-
-        KafkaStreams streams = new KafkaStreams(builder.build(), props);
-        streams.start();
+        KafkaStreamsConfiguration streams = new KafkaStreamsConfiguration(props);      
         return streams;
     }
 }
