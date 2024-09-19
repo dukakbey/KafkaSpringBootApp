a3af8185a6671e0a5d7331e7ef77ff344422396c
dukakbey <96448595+dukakbey@users.noreply.github.com>
2024-09-19 11:05:01 +0300

    stream added and run without error.

--- kafka-data-receiver/src/main/java/org/sceylan/kafka_data_receiver/service/KafkaConsumerService.java
+++ kafka-data-receiver/src/main/java/org/sceylan/kafka_data_receiver/service/KafkaConsumerService.java
@@ -5,15 +5,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumerService {
  
+    // // @KafkaListener(topics = "customer_data")
     // @KafkaListener(topics = "customer_data")
-    @KafkaListener(topics = "customer_data")
-    public static void receiveData(String m)
-    {
-        System.out.println("Received Messagee: " + m);
-    }
+    // public static void receiveData(String m)
+    // {
+    //     System.out.println("Received Message customer_data: " + m);
+    // }
     @KafkaListener(topics = "updated_data")
     public static void receiveUptdatedData(String m)
     {
-        System.out.println("Received Message: " + m);
+        try {
+            // String olarak alınan mesajı Long'a dönüştür
+            Long length = Long.parseLong(m);
+            System.out.println("Received Message updated_data: " + length);
+        } catch (NumberFormatException e) {
+            System.err.println("Error parsing message: " + m);
+        }
+     
     }
 }
--- spring-kafka-streams/src/main/java/org/sceylan/spring_kafka_streams/service/WordCountProcessor.java
+++ spring-kafka-streams/src/main/java/org/sceylan/spring_kafka_streams/service/WordCountProcessor.java
@@ -9,25 +9,45 @@ import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.kstream.ValueMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.stereotype.Component;
+
 @Component
+@EnableKafka
+@EnableKafkaStreams
 public class WordCountProcessor {
+  private static final Serde<String> STRING_SERDE = Serdes.String();
+  private static final Serde<Long> LONG_SERDE = Serdes.Long();
 
-    private static final Serde<String> STRING_SERDE = Serdes.String();
+  @Autowired
+  void buildPipeline(StreamsBuilder streamsBuilder) {
+    KStream<String, String> messageStream = streamsBuilder
+        .stream("customer_data", Consumed.with(STRING_SERDE, STRING_SERDE));
 
-    @Autowired
-    void buildPipeline(StreamsBuilder streamsBuilder) {
-        KStream<String, String> messageStream = streamsBuilder
-          .stream("customer_data", Consumed.with(STRING_SERDE, STRING_SERDE));
+    KStream<String, Long> lengthCounts = messageStream
+        .mapValues((ValueMapper<String, Long>) value -> (long) value.length());
 
-        KTable<String, Long> wordCounts = messageStream
-          .mapValues((ValueMapper<String, String>) String::toLowerCase)
-          .flatMapValues(value -> Arrays.asList(value.split("\\W+")))
-          .groupBy((key, word) -> word, Grouped.with(STRING_SERDE, STRING_SERDE))
-          .count();
+    lengthCounts.to("updated_data", Produced.with(STRING_SERDE, LONG_SERDE));
+  }
 
-        wordCounts.toStream().to("updated_data");
-    }
+  // private static final Serde<String> STRING_SERDE = Serdes.String();
+
+  // @Autowired
+  // void buildPipeline(StreamsBuilder streamsBuilder) {
+  // KStream<String, String> messageStream = streamsBuilder
+  // .stream("customer_data", Consumed.with(STRING_SERDE, STRING_SERDE));
+
+  // KTable<String, Long> wordCounts = messageStream
+  // .mapValues((ValueMapper<String, String>) String::toLowerCase)
+  // .flatMapValues(value -> Arrays.asList(value.split("\\W+")))
+  // .groupBy((key, word) -> word, Grouped.with(STRING_SERDE, STRING_SERDE))
+  // .count();
+
+  // wordCounts.toStream().to("updated_data");
+
+  // }
 }
